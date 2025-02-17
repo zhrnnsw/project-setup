64781d37ca6183b755133b98bd718778a0831bb3
Zahra Annisa Wahono <zahraannisawahono@gmail.com>
2025-02-17 15:25:24 +0700

    refactor: env services

--- src/Services/EnvironmentManager.php
+++ src/Services/EnvironmentManager.php
@@ -12,9 +12,26 @@ class EnvironmentManager
             return false;
         }
 
+        // Mapping manual key ke format .env yang benar
+        $keyMapping = [
+            'db_port' => 'DB_PORT',
+            'db_database' => 'DB_DATABASE',
+            'db_username' => 'DB_USERNAME',
+            'db_password' => 'DB_PASSWORD',
+        ];
+
         $envContent = File::get($envPath);
+
         foreach ($data as $key => $value) {
-            $envContent = preg_replace("/^{$key}=.*/m", "{$key}={$value}", $envContent);
+            // Cek jika key ada dalam mapping, kalau tidak tambahkan manual
+            if (isset($keyMapping[$key])) {
+                $envKey = $keyMapping[$key];
+                // Update atau tambah key jika sudah ada
+                $envContent = preg_replace("/^{$envKey}=\s*.*/m", "{$envKey}={$value}", $envContent);
+            } else {
+                // Tambahkan key-value baru jika tidak ada dalam mapping
+                $envContent .= "\n{$key}={$value}";
+            }
         }
 
         File::put($envPath, $envContent);
